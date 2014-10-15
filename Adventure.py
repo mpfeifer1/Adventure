f05e951d6ac22db888cec2e13d23fa862659d670
Jacob Pfeifer <jakepf00@gmail.com>
2014-10-14 21:16:37 -0500

    We added credit to the author!

--- Adventure.py
+++ Adventure.py
@@ -1,3 +1,4 @@
 print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("Welcome to the Mountain of Mysteriousness")
 print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print ("~~~By~J~Cornelius~Fifelolz~~~~~~~~~~~~~~~")
