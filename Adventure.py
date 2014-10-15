745858a451576d28303534015f272961035c3ed3
Jacob Pfeifer <jakepf00@gmail.com>
2014-10-14 21:23:47 -0500

    I added a user prompt!

--- Adventure.py
+++ Adventure.py
@@ -2,3 +2,7 @@ print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("Welcome to the Mountain of Mysteriousness")
 print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("~~~By~J~Cornelius~Fifelolz~~~~~~~~~~~~~~~")
+print("""hi man what do ya do
+1...eat
+2...fight
+3...kill""")
