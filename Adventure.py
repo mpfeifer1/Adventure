8f75326be18dabb9e4b5bc8190ac0790095c3879
mpfeifer <mikepf97@gmail.com>
2014-10-15 21:21:15 -0500

    Added first user input if/elif/else

--- Adventure.py
+++ Adventure.py
@@ -2,7 +2,16 @@ print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("Welcome to the Mountain of Mysteriousness")
 print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("~~~By~J~Cornelius~Fifelolz~~~~~~~~~~~~~~~")
-print("""hi man what do ya do
-1...eat
-2...fight
-3...kill""")
+
+print
+print ("You are finished preparing for your journey!")
+print ("Do you leave home in search of adventure?")
+response = raw_input("Y or N: ")
+if response == ("y"):
+   print ("Cool!")
+elif response == ("n"):
+   print ("Lame!")
+else:
+   print ("Dolt...")
+
+print ("Game Over")
