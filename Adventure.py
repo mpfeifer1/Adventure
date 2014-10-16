8de92969d26be9435b966ff4b3adca16008bad33
mpfeifer <mikepf97@gmail.com>
2014-10-15 21:35:25 -0500

    Created a Situation method based on existing code, and used examples.

--- Adventure.py
+++ Adventure.py
@@ -1,17 +1,22 @@
+print
 print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("Welcome to the Mountain of Mysteriousness")
 print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("~~~By~J~Cornelius~Fifelolz~~~~~~~~~~~~~~~")
-
 print
-print ("You are finished preparing for your journey!")
-print ("Do you leave home in search of adventure?")
-response = raw_input("Y or N: ")
-if response == ("y"):
-   print ("Cool!")
-elif response == ("n"):
-   print ("Lame!")
-else:
-   print ("Dolt...")
+print
 
-print ("Game Over")
+def situation(question, yes, no):
+	print (question)
+	response = raw_input("Y or N: ")
+	if response == ("y"):
+		print (yes)
+	elif response == ("n"):
+		print (no)
+	else:
+		print ("Dolt...")
+	print
+
+situation("You are finished preparing for your journey! Do you leave home in search of adventure?" , "Cool!" , "Too Bad!")
+situation("You are hungry. Do you eat?" , "Cool!" , "That's gonna suck in like an hour or so...")
+print ("Game Over!")
